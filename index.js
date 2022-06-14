a9f9972f5c12a8b2db2d73bce9f8c837bf996510
Andy Park <apark1214@gmail.com>
2022-06-13 17:08:39 -0700

    working on Js problems

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 
-let i=0;
-const increment =()=>{
-  i+=2;
-  console.log('outside function',i)
-  const incrementTwo =()=>{
-    setTimeout(()=>{
-         i += 3;
-         console.log("inside function", i);
-    },3000)
-  }
-  incrementTwo();
-};
 
-increment();
-i+=1;
-console.log('outside',i);
+
+// function outer(){
+//   console.log('outer')
+//   function inner(){
+//     for (var i=0;i<5;i++){
+//           // debugger;
+//       i+=1;
+//       console.log('inner',i)
+//     }
+//   }
+//   inner();
+// }
+
+
+// function testing(){
+//   i = 0;
+//   for (let i=0;i<5;i++){
+//      i += 1;
+//     console.log('testing',i)
+//   }
+// }
+
+// // outer();
+// testing()
+
+var myObject = {
+  foo: "bar",
+  func: function () {
+    var self = this;
+    console.log("outer func:  this.foo = " + this.foo);
+    console.log("outer func:  self.foo = " + self.foo);
+    (function () {
+      console.log("inner func:  this.foo = " + this.foo);
+      console.log("inner func:  self.foo = " + self.foo);
+    })();
+  },
+};
+myObject.func();
